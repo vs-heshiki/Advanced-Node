1d124fb4af0973a13f280093fa2f8821ec38bbe5
Victor Heshiki <victor_heshiki@hotmail.com>
2023-03-02 17:40:17 -0300

    add lint-staged and husky

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-    moduleNameMapper: {
-        '@/tests/(.+)': '<rootDir>/tests/$1',
-        '@/(.+)': '<rootDir>/src/$1'},
-    roots: [
-        '<rootDir>/src',
-        '<rootDir>/tests'
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
   ],
-    transform: {
-        '\\.ts$': 'ts-jest'
-    }
-};
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
