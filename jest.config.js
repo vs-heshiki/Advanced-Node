ae056be93f0a8b23bdc2f10eabbeab5dbb6ecc17
Victor Heshiki <victor_heshiki@hotmail.com>
2023-03-17 21:39:19 -0300

    add jest config to integration tests

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
   },
+  testMatch: [
+    '**/*.spec.ts'
+  ],
   roots: [
     '<rootDir>/src',
     '<rootDir>/tests'
