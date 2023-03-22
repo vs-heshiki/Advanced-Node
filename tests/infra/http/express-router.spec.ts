879da838995a5754308f0e0505a8b48f1db66d91
Victor Heshiki <victor_heshiki@hotmail.com>
2023-03-22 17:00:49 -0300

    ensure ExpressRouter respond with 200 and correct data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -2,18 +2,41 @@ import { Controller } from '@/application/controllers'
 import { Request, Response } from 'express'
 
 import { getMockReq, getMockRes } from '@jest-mock/express'
-import { mock } from 'jest-mock-extended'
+import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('ExpressRouter', () => {
-    it('should call handle with correct request', async () => {
-        const controller = mock<Controller>()
-        const req = getMockReq({ body: { any: 'any' } })
-        const res = getMockRes().res
-        const sut = new ExpressRouter(controller)
+    let controller: MockProxy<Controller>
+    let req: Request
+    let res: Response
+    let sut: ExpressRouter
 
+    beforeEach(() => {
+        req = getMockReq({ body: { any: 'any' } })
+        res = getMockRes().res
+        controller = mock()
+        controller.handle.mockResolvedValue({
+            statusCode: 200,
+            data: {
+                any: 'any_data'
+            }
+        })
+        sut = new ExpressRouter(controller)
+    })
+
+    it('should call handle with correct request', async () => {
         await sut.adapt(req, res)
 
         expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+        expect(controller.handle).toHaveBeenCalledTimes(1)
+    })
+
+    it('should respond with 200 and correct data', async () => {
+        await sut.adapt(req, res)
+
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.status).toHaveBeenCalledTimes(1)
+        expect(res.json).toHaveBeenCalledWith({ any: 'any_data' })
+        expect(res.json).toHaveBeenCalledTimes(1)
     })
 })
 
@@ -21,6 +44,7 @@ export class ExpressRouter {
     constructor (private readonly controller: Controller) { }
 
     async adapt (req: Request, res: Response): Promise<void> {
-        await this.controller.handle({ ...req.body })
+        const httpResponse = await this.controller.handle({ ...req.body })
+        res.status(200).json(httpResponse.data)
     }
 }
