a3cb8a04f151949f44e6e9bd35692f74d13917b6
Victor Heshiki <victor_heshiki@hotmail.com>
2023-03-28 16:42:20 -0300

    create type alias to ExpressRouter Adapter

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -2,10 +2,10 @@ import { Controller } from '@/application/controllers'
 
 import { RequestHandler } from 'express'
 
-export const adaptExpressRouter = (controller: Controller): RequestHandler => {
-    return (async (req, res) => {
-        const { statusCode, data } = await controller.handle({ ...req.body })
-        const json = statusCode === 200 ? data : { error: data.message }
-        res.status(statusCode).json(json)
-    }) as RequestHandler
+type Setup = (controller: Controller) => RequestHandler
+
+export const adaptExpressRouter: Setup = controller => async (req, res) => {
+    const { statusCode, data } = await controller.handle({ ...req.body })
+    const json = statusCode === 200 ? data : { error: data.message }
+    res.status(statusCode).json(json)
 }
